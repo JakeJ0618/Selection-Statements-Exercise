b51796c6c230388c36a73bde3ce769fbcd7357bd
Jake Johnston <jakejohnston618@gmail.com>
2023-03-08 14:43:25 -0600

    made guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 100);
+            
+            Console.WriteLine("Guess a number, any number between 1 and 100!");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+
+
+
+
         }
+
     }
-}
+}        
+             
+                
+
+          
+            
+                
+
+            
+
+            
+
+
+
+        
+        
+
